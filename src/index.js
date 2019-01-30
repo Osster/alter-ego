c978b36e246e4b3e5fb54d771cba115045687518
Dada <dada0net@gmail.com>
2019-01-30 19:52:17 +0300

    kat 30/01

--- src/index.js
+++ src/index.js
@@ -18,21 +18,22 @@ const wow = new WOW(
 );
 wow.init();
 
-
 new Vue({
 	el: '#wrapper',
 	data: {
 		pageScroll: 0,
+		pageScrollDraw: 0,
 		mainNavigation: false,
 		mainNavigationStyle: '',
 		mainNavigationLi: false,
 		mainNavigationStyleLi: 'opacity: 0; transform: matrix(1, 0, 0, 1, 0, -180); transition:  all 0.5s ease;',
 		showLanding: true,
+		drawHover: true,
 		items: [
-			'event',
-			'content',
-			'digital',
-			'contact'
+			{name: 'event', url: '/event.html', delay: '1s'},
+			{name: 'content', url: '/content.html', delay: '2s'},
+			{name: 'digital', url: '/digital.html', delay: '3s'},
+			{name: 'contact', url: '/contact.html', delay: '4s'}
 		]
 	},
 	mounted() {
@@ -47,8 +48,18 @@ new Vue({
 				this.showLanding = true;
 			}
 		},
+		pageScrollDraw(val) {
+			if (val *0.6 > window.outerHeight) {
+				this.drawHover = false;
+			} else {
+				this.drawHover = true;
+			}
+		},
 	},
 	methods: {
+		shuffle: function () {
+			this.itemscust = _.shuffle(this.itemscust)
+		},
 		handleMenu() {
 			const body = document.body;
 			this.mainNavigation = !this.mainNavigation;
@@ -58,8 +69,23 @@ new Vue({
 			this.mainNavigationLi = !this.mainNavigationLi;
 
 			this.mainNavigationStyleLi = this.mainNavigationLi
-				? 'opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0); transition:  all .5s ease;'
+				? 'opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0); transition:  all .5s ease; transition-delay: 0.5s;'
 				: 'opacity: 0; transform: matrix(1, 0, 0, 1, 0, -180); transition:  all .5s ease;';
+
+			// this.items.length
+			// var p = this.items.length;
+			// for (let j = 1; j <= p; j++) {
+			//
+			// 	this.mainNavigationStyleLi = this.mainNavigationLi
+			// 		? 'opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0); transition:  all .5s ease; transition-delay:' + 0.5*j + 's;'
+			// 		: 'opacity: 0; transform: matrix(1, 0, 0, 1, 0, -180); transition:  all .5s ease;';
+			// }
+
+			// this.items.forEach (
+			// 	this.mainNavigationStyleLi = this.mainNavigationLi
+			// 		? 'opacity: 1; transform: matrix(1, 0, 0, 1, 0, 0); transition:  all .5s ease; transition-delay:' + 0.5 + 's;'
+			// 		: 'opacity: 0; transform: matrix(1, 0, 0, 1, 0, -180); transition:  all .5s ease;'
+			// );
 
 			if (this.mainNavigation) {
 				body.classList.add('navigation-visible');
@@ -68,33 +94,9 @@ new Vue({
 			}
 		},
 
-		// timeout: function () {
-		// 	setTimeout(function () { this.handleMenu() }.bind(this), 1000)
-		// },
-
 		handlePageScroll(e) {
 			this.pageScroll = e.pageY;
-		},
+			this.pageScrollDraw = e.pageY;
+		}
 	},
 });
-
-// // global menu
-// if ($('#blog-detail .blogs-photo').length > 0) {
-// 	if (this.pageYOffset <= $('#blog-detail .blogs-photo').height()) {
-// 		$('.header-right').removeClass('ms-active');
-// 	} else {
-// 		$('.header-right').addClass('ms-active');
-// 	}
-// } else {
-// 	$('.header-right').removeClass('ms-active');
-// }
-// if ($('#project-detail .project-photo').length > 0) {
-// 	if (this.pageYOffset <= $('#project-detail .project-photo').height()) {
-// 		$('.header-right').removeClass('ms-active');
-// 	} else {
-// 		$('.header-right').addClass('ms-active');
-// 	}
-// } else {
-// 	$('.header-right').removeClass('ms-active');
-// }
-// // global menu
